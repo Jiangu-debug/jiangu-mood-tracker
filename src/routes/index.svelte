b284c81bf712bab9d7cf398a5f1f87de040b4eaa
Jiangu-debug <clhs.d5174@gmail.com>
2022-01-09 04:01:20 +0000

    mood-tracker

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+
+
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>	
+
+<script>
+    import supabase from '$lib/db';
+
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
